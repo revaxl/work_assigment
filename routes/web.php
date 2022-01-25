0c0526115991219253acaf7f6e7a5eb3c6cfb347
revaxl <revaxl09@gmail.com>
2022-01-25 16:33:45 +0900

    add auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
