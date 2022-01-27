ac59dc4b959a716c75dc9f332b1a8b3dabfc2bbb
revaxl <revaxl09@gmail.com>
2022-01-27 16:09:10 +0900

    create login page

---
+++ resources/views/components/auth/login.blade.php
@@ -0,0 +1,70 @@
+@extends('base')
+@section('content')
+<div class="flex items-center justify-center h-screen">
+    <div class="w-full max-w-md">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <div class="flex bg-red-100 rounded-lg p-4 mb-4 text-sm text-red-700" role="alert">
+                    <svg class="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                    <div>
+                        {{ $error }}
+                    </div>
+                </div>
+            @endforeach
+        @endif
+        <form class="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4" action="{{ route('auth.login') }}" method="POST">
+            @csrf
+            <div
+                class="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4"
+            >
+                Login
+            </div>
+            <div class="mb-4">
+                <label
+                    class="block text-gray-700 text-sm font-normal mb-2"
+                    for="email"
+                >
+                    Email
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="email"
+                    type="email"
+                    id="email"
+                    required
+                    autofocus
+                    placeholder="Email"
+                />
+            </div>
+            <div class="mb-6">
+                <label
+                    class="block text-gray-700 text-sm font-normal mb-2"
+                    for="password"
+                >
+                    Password
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    id="password"
+                    required
+                    autocomplete="current-password"
+                />
+            </div>
+            <div class="flex items-center">
+                <button
+                    class="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700"
+                    type="submit">Log In
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
