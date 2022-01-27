7ed0589faf69285a01fbb665026bd5628abbeb22
revaxl <revaxl09@gmail.com>
2022-01-27 16:06:21 +0900

    add base layout

---
+++ resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous"></script>
+</head>
+<body class="bg-gray-100">
+    @yield('content')
+</body>
+</html>
