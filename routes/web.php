b3656c3351d7bdac555527e1a08dca4aced4140d
revaxl <revaxl09@gmail.com>
2022-01-27 16:13:39 +0900

    add the necessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/','components.auth.login')->name('home');
+
+// User pages
+Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
+    Route::get('mypage', [UserController::class, 'index'])->name('user.index');
+    Route::post('update', [UserController::class, 'update'])->name('user.update');
 });
 
+// Auth
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('registration', [AuthController::class, 'registration'])->name('auth.registerForm');
 Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
