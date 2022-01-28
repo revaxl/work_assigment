5674d7188f2118ed65bb8e33e1f4abf8f691ab2c
revaxl <revaxl09@gmail.com>
2022-01-28 10:17:15 +0900

    add login method to base test class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,18 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Auth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function login()
+    {
+        $user = User::factory()->create();
+        Auth::login($user);
+        return $user;
+    }
 }
